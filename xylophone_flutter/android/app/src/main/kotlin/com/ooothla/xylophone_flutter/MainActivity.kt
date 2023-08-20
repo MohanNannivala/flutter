f0f72189ef9e320b1ff65a3176a435478618f0b2
Mohan NS <mohannannivala@yahoo.com>
2023-08-20 21:39:22 +0530

    Xylophone App
    Signed-off-by: Mohan NS <mohannannivala@yahoo.com>

---
+++ xylophone_flutter/android/app/src/main/kotlin/com/ooothla/xylophone_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ooothla.xylophone_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ xylophone_flutter/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:audiofileplayer/audiofileplayer.dart';
+
+void main() => runApp(const XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  Expanded buildKey({required Color color, required int soundNumber}) {
+    return Expanded(
+        child: TextButton(
+        onPressed: () {
+          Audio audio = Audio.load('assets/note$soundNumber.wav');
+          audio.play();
+        },
+          style: TextButton.styleFrom(
+              backgroundColor: color,
+          ),
+          child: const Text(""),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
